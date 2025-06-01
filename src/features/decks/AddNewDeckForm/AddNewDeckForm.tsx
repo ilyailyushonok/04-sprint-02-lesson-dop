dde3a9013cfd3946d4b59a055d946be2fcbb1f65
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-06-01 23:23:24 +0300

    reset input AddNewDeckForm

--- src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
+++ src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
@@ -11,6 +11,7 @@ export const AddNewDeckForm = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<FormValues>({
     defaultValues: {
@@ -20,6 +21,7 @@ export const AddNewDeckForm = () => {
   const dispatch = useAppDispatch()
   const onSubmit = (data: FormValues) => {
     dispatch(addDeskTC(data))
+    reset()
   }
 
   return (
